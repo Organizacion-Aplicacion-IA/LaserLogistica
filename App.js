b03b2daadf23a70e7a1d55408c3ed794dc97b4fa
Fernando2341 <ordazfer735@gmail.com>
2025-02-21 22:20:44 -0600

    (funcionalidad): Agregando Expo y Gluestack.io

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "red"}}>texto prueba</Text>
-      <StatusBar style="light" />
-      <Button title="Button" onPress={() => alert("Soy una alerta")}/>
-    </View>
+    <GluestackUIProvider mode="light"><View style={styles.container}>
+        <Text style={{ color: "red"}}>texto prueba</Text>
+        <StatusBar style="light" />
+        <Button title="Button" onPress={() => alert("Soy una alerta")}/>
+      </View></GluestackUIProvider>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
